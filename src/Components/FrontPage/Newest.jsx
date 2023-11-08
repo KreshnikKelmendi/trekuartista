81639ea241428779ed2f29e40cd67c47a259cda1
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-11-07 17:14:15 -1200

    lastChange

--- src/Components/FrontPage/Newest.jsx
+++ src/Components/FrontPage/Newest.jsx
@@ -38,14 +38,14 @@ const Newest = () => {
   const [hoveredIndex, setHoveredIndex] = useState(-1);
 
   return (
-    <div className="py-0 md:py-4 lg:px-[50px]">
+    <div className="py-0 md:py-2 lg:px-[50px]">
       <div className="px-4 flex mt-14 items-center">
         <h1 className="text-4xl md:text-5xl lg:text-[33px] font-bold font-custom leading-[40px]">
           NEWEST
         </h1>
         <img src={pinkLogo} className="w-8 md:w-10 lg:w-[40px] h-8 md:h-10 lg:h-[34px] mt-3 ml-4 object-cover" alt="" />
       </div>
-      <div className="grid lg:grid-cols-2 gap-y-16 mx-2 mt-[50px]">
+      <div className="grid lg:grid-cols-2 gap-y-28 mx-2 mt-[50px]">
         {images.map((image, index) => (
           <div
             key={index}
@@ -54,7 +54,7 @@ const Newest = () => {
             onMouseLeave={() => setHoveredIndex(-1)}
           >
             <p className='font-custom1 text-base text-[#979797] lg:w-[60vh]'>{image.categories}</p>
-            <div className="relative w-full h-[405px] mt-4">
+            <div className="relative w-full h-full mt-4">
               <img
                 src={image.url}
                 alt=""
@@ -87,7 +87,7 @@ const Newest = () => {
         ))}
       </div>
       <div className='justify-center items-center text-center'>
-        <button className="mt-[60px] w-[207px] text-black hover-bg-black transition duration-500 ease-in-out hover:text-white text-base border border-[#1E1E1E] font-custom1 py-2 px-4">
+        <button className="mt-[120px] w-[207px] text-black hover-bg-black transition duration-500 ease-in-out hover:text-white text-base border border-[#1E1E1E] font-custom1 py-2 px-4">
           View more
         </button>
       </div>
