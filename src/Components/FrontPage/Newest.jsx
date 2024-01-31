40e9e6a48219021adde1689a5f303ba9b3ad50aa
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-01-30 13:35:39 -1200

    free

--- src/Components/FrontPage/Newest.jsx
+++ src/Components/FrontPage/Newest.jsx
@@ -62,8 +62,8 @@ const Newest = () => {
               </Link>
               <div className="relative w-full h-full mt-4">
                 {item?.workImage?.endsWith('.mp4') ? (
-                  <video className="w-full h-[40vh] lg:h-[504px] object-cover" autoPlay playsInline loop muted>
-                    <source src={item?.workImage} />
+                  <video className="w-full h-[40vh] lg:h-[504px] object-cover" autoPlay controls playsInline loop muted>
+                    <source src={item?.workImage} type="video/mp4" />
                   </video>
                 ) : (
                   <img
